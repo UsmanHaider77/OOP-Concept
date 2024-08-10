d7d4f7c7bc733f78609c04ce7eeb49c9db0c1660
UsmanHaider77 <musmanhaider77@gmail.com>
2024-08-10 10:48:59 +0500

    OOP Concept In Dart

---
+++ oop_concept.dart
@@ -0,0 +1,82 @@
+// void main(){
+//   Person obj = Person("Usman", "22");
+//   obj.display();
+// }
+
+// class Person{
+//   String name;
+//   String age;
+//   Person(this.name,this.age);
+
+//   display(){
+//     print("Name : $name");
+//     print("Age : $age");
+//   }
+// }
+
+//----------------------------(Inheritance)-------------------------------//
+
+// void main() {
+//   Student obj = Student();
+//   obj.name = "Usman";
+//   obj.age = "22";
+//   obj.display();
+// }
+
+// class Person {
+//   String? name;
+//   String? age;
+//   display() {
+//     print("Name : $name");
+//     print("Age : $age");
+//   }
+// }
+
+// class Student extends Person {}
+
+// class Teacher {}
+
+//----------------------------(Polymorphism)-------------------------------//
+
+// void main() {
+//   Student obj = Student();
+//   obj.name = "Usman";
+//   obj.age = "22";
+//   obj.display();
+// }
+
+// class Person {
+//   String? name;
+//   String? age;
+//   display() {
+//     print("Name : $name");
+//     print("Age : $age");
+//   }
+// }
+
+// class Student extends Person {
+
+//     @override
+
+//   String? get name => super.name = "Haider";
+
+//   @override
+//   display() {
+//     print("Student me overriding hogai : $name");
+//     super.display();
+//   }
+// }
+
+// class Teacher {}
+
+//----------------------------(Encampsulation)-------------------------------//
+
+import 'person.dart';
+
+void main() {
+  Student obj = Student ();
+  obj.name;
+  obj._bankDetails;
+}
+
+class Student extends Person {}
---
+++ person.dart
@@ -0,0 +1,8 @@
+class Person {
+  String name = "Usman";
+  String _bankDetails = "abc-abc-abc";
+  display() {
+    print("Name : $name");
+    print("Age : $_bankDetails");
+  }
+}
